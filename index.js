b17bd1b36d9923e81ad69f96cbc09f565c7a44aa
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-08-11 17:12:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Gatfield"] //define your array here
 
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
 // Add your functions and code here
